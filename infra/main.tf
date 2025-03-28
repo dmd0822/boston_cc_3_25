d083953cdeb6866b5b83bf2b3e8eb6c29ee04a6d
Dave Davis <dave@davemdavis.net>
2025-03-28 14:25:27 -0400

    Rename resource from azurerm_app_service_plan to azurerm_service_plan and update configuration for Linux OS type

--- infra/main.tf
+++ infra/main.tf
@@ -7,21 +7,19 @@ data "azurerm_resource_group" "rg" {
   name = "rg-cc-3-29-25"
 }
 
-resource "azurerm_app_service_plan" "asp" {
+resource "azurerm_service_plan" "asp" {
   name                = "blazor-app-service-plan"
   location            = data.azurerm_resource_group.rg.location
   resource_group_name = data.azurerm_resource_group.rg.name
-  sku {
-    tier = "Free"
-    size = "F1"
-  }
+  os_type   = "Linux"
+  sku_name  = "F1"
 }
 
 resource "azurerm_app_service" "app" {
   name                = "blazor-app-service"
   location            = data.azurerm_resource_group.rg.location
   resource_group_name = data.azurerm_resource_group.rg.name
-  app_service_plan_id = azurerm_app_service_plan.asp.id
+  app_service_plan_id = azurerm_service_plan.asp.id
 
   site_config {
     dotnet_framework_version = "v6.0"
