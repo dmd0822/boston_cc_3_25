f73c70f2c57363eca81276d382babff7e698236a
Dave Davis <dave@davemdavis.net>
2025-03-28 13:31:13 -0400

    Remove Terraform configuration files for Blazor app service

--- outputs.tf
+++
@@ -1,4 +0,0 @@
-output "app_service_url" {
-  description = "The URL of the Blazor App Service"
-  value       = azurerm_app_service.app.default_site_hostname
-}
--- variables.tf
+++
@@ -1,4 +0,0 @@
-variable "app_name" {
-  description = "Name of the Blazor app"
-  default     = "blazor-app-service"
-}
