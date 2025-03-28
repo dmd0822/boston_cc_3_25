8664a0edd1595189a9ee475f46fc7cce2de21a1b
Dave Davis <dave@davemdavis.net>
2025-03-28 13:26:22 -0400

    Comment out app_service_url output in Terraform configuration

--- infra/main.tf
+++ infra/main.tf
@@ -31,6 +31,6 @@ resource "azurerm_app_service" "app" {
   }
 }
 
-output "app_service_url" {
-  value = azurerm_app_service.app.default_site_hostname
-}
+# output "app_service_url" {
+#   value = azurerm_app_service.app.default_site_hostname
+# }
