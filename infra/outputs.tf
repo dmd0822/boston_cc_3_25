e93b76f0550e50b18a44824df01748dda2a1d3c8
Dave Davis <dave@davemdavis.net>
2025-03-28 14:31:37 -0400

    Update output to reference Linux Web App hostname

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,4 +1,4 @@
 output "app_service_url" {
   description = "The URL of the Blazor App Service"
-  value       = azurerm_app_service.app.default_site_hostname
+  value       = azurerm_linux_web_app.app.default_site_hostname
 }
